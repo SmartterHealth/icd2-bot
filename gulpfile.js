fd0eee505dce2ecb188ba5d660d4404b450ec0a1
TylerDurham <TylerDurham@users.noreply.github.com>
2019-06-11 15:49:34 -0500

    code cleanup/comments
    --nopatch flag used to supress incrementing build number.

--- gulpfile.js
+++ gulpfile.js
@@ -8,41 +8,67 @@ var bump = require('gulp-bump');
 var argv = require('yargs').argv;
 var gulpif = require('gulp-if');
 
-function patchVersion(done) {
+/**
+ * Increments the build version by 1. Use the --nopatch flag to supress.
+ */
+const patch = function() {
     return gulp.src('./package.json')
-        .pipe(gulpif(!argv.nobump, bump()))
+        .pipe(gulpif(!argv.nopatch, bump()))
         .pipe(gulp.dest('./'));
 }
+patch.description = 'Increments the build version by 1. Use the --nopatch flag to supress.';
 
+/**
+ * Copies all non-typescript files into output directory.
+ */
 function copy() {
     return gulp.src(['./src/**/*.json'])
         .pipe(gulp.dest('./lib'));
 }
+copy.description = 'Copies all non-typescript files into output directory.';
 
-function clean() {
+/**
+ * Cleans all compiled TypeScript files.
+ */
+const clean = function() {
     return del('./lib');
 }
 clean.description = 'Cleans all compiled TypeScript files.';
 
-function docs() {
+/**
+ * Converts README.docx to README.md (markdown).
+ */
+const docs = function() {
     // REQUIRES pandoc!!! https://pandoc.org/MANUAL.html
     return exec('pandoc -s README.docx -t markdown -o README.md')
 }
+docs.description = 'Converts README.docx to README.md (markdown).';
 
-function tsc() {
+/**
+ * Compiles all TypeScript.
+ */
+const tsc = function() {
     return exec('tsc');
 }
-tsc.description = "Compiles all TypeScript."
+tsc.description = "Compiles all TypeScript.";
 
-const watch = (done) => {
+/**
+ * Runs watch on all files. Runs gulp serve when changes occur.
+  */
+const watch = () => {
     return gulp.watch(['./src/**/*.*'], gulp.series(serve));
 }
+watch.description = 'Runs watch on all files. Runs gulp serve when changes occur.';
 
-function zip() {
+/**
+ * Compresses all *.js files in the deployment folder.
+ */
+const zip = function() {
     return gulp.src('./deployment/package/**/*.*')
         .pipe(ziplib('icd2-bot.zip', { compress: true }))
         .pipe(gulp.dest('./deployment'));
 }
+zip.description = 'Compresses all *.js files in the deployment folder.';
 
 function start(done) {
     return nodemon({
@@ -79,15 +105,22 @@ const packageAzureFiles = [
     '!./**/*.map'
 ]
 
-function package(done) {
+/**
+ * Copies all *.js files into the ./deployment folder.
+  */
+const package = function() {
     return del('./deployment')
         .then((value) => {
             gulp.src(packageAzureFiles)
                 .pipe(gulp.dest('./deployment/package'));
         });
 }
+package.description = 'Copies all *.js files into the ./deployment folder.'
 
-const build = gulp.series(clean, copy, docs, tsc, patchVersion, zip, package);
+const build = gulp.series(clean, copy, docs, tsc, patch, zip, package);
+build.description = 'Performs the entire build process.'
+
 const serve = gulp.series(build, start);
+serve.description = 'Runs the build, then starts the bot.'
 
-module.exports = { copy, clean, build, docs, serve, increment: patchVersion, tsc, watch, zip, package }
+module.exports = { copy, clean, build, docs, serve, patch, tsc, watch, zip, package }
